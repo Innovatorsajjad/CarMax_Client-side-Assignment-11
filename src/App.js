1ebce6cf4f71aae6c7a925504531b201d6877141
Khandaker Sajjad Hossen <sk.sajjad4207@gmail.com>
2022-05-19 15:22:56 +0600

    footer added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Footer from './components/Footer/Footer';
 import Navbar from './components/Navbar/Navbar';
 import Blogs from './Pages/Blogs/Blogs';
 import Home from './Pages/Home/Home';
@@ -16,6 +17,7 @@ function App() {
       <Route path='/manageinventory' element={<ManageInventory/>}></Route>
       <Route path='/blogs' element={<Blogs/>}></Route>
     </Routes>
+    <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Footer = () => {
+    return (
+    <footer className="text-center text-white mt-5 bg-dark p-5">
+
+        <p >© copyright by Sajjad</p>
+
+        </footer>
+    );
+};
+
+export default Footer;
