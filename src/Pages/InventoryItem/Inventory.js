d56123bf99f48292cb655c99c74db0d84b859cbb
Khandaker Sajjad Hossen <sk.sajjad4207@gmail.com>
2022-05-26 20:47:51 +0600

    get all products

--- src/Pages/InventoryItem/Inventory.js
+++ src/Pages/InventoryItem/Inventory.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const Inventory = ({product}) => {
-    const { name, price, img, description, _id, ratings } = product;
+    const { name, price, img, description, _id, supplier,quantity } = product;
     const navigate = useNavigate();
     const handleUpdate = () => {
         navigate(`/inventory/${_id}`)
@@ -14,11 +14,11 @@ const Inventory = ({product}) => {
                     <img src={img} className="card-img-top w-100" alt="..." />
                     <div className="card-body">
                         <h3 className="card-title">{name.slice(0, 18)}</h3>
-                        {/* {
-                            supplier ? <h5>Supplier: {supplier}</h5> : <h5>Supplier: Secret Supplier</h5>
-                        } */}
+                        {
+                            supplier ? <h5>Supplier: {supplier.slice(0,16)}</h5> : <h5>Supplier: Secret Supplier</h5>
+                        }
                         <h5>Price: {price}</h5>
-                        <h6>Available: {ratings}</h6>
+                        <h6>Available: {quantity}</h6>
                         <p className="card-text">{description.slice(0, 68)}...</p>
                         <button onClick={handleUpdate} className="btn btn-primary " id='inventory-btn'>Update QTY</button>
                     </div>
