5cd8b82a3a32702b871a31b21648af626ab92307
Khandaker Sajjad Hossen <sk.sajjad4207@gmail.com>
2022-05-27 07:28:47 +0600

    blogs updated firs time

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -11,31 +11,46 @@ const Blogs = () => {
                     <div className="accordion-item">
                         <h2 className="accordion-header" id="flush-headingOne">
                             <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-                                Accordion Item #1
+                                1. Difference between javascript and nodejs
                             </button>
                         </h2>
                         <div id="flush-collapseOne" className="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-                            <div className="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion body.</div>
+                            <div className="accordion-body">JavaScript is a programming language. It is running in any web browser with a proper browser engine.
+
+                                NodeJs is an interpreter and environment for JavaScript with some specific useful libraries which JavaScript programming can use separately.</div>
                         </div>
                     </div>
                     <div className="accordion-item">
                         <h2 className="accordion-header" id="flush-headingTwo">
                             <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-                                Accordion Item #2
+                                2. When should we use nodejs and when should we use mongodb
                             </button>
                         </h2>
                         <div id="flush-collapseTwo" className="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
-                            <div className="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the second item's accordion body. Let's imagine this being filled with some actual content.</div>
+                            <div className="accordion-body">NodeJs is an asynchronous event-driven JavaScript runtime. We will use it to create a server.
+
+                                MongoDB is database it is used to storing data. So, when we need to store our data we should use MongoDB.</div>
                         </div>
                     </div>
                     <div className="accordion-item">
                         <h2 className="accordion-header" id="flush-headingThree">
                             <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-                                Accordion Item #3
+                                3. Differences between sql and nosql databases
                             </button>
                         </h2>
                         <div id="flush-collapseThree" className="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
-                            <div className="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
+                            <div className="accordion-body">
+                                <h3>SQL Database</h3><br />
+                                1.SQL database is a relational database system.<br />
+                                2.SQL database have static or pre-defined schema.<br />
+                                3.SQL database are best suited for complex queries.<br />
+
+                                <h3>NoSQL Database</h3>
+                                1.NoSQL database is a non-relational database system. <br />
+                                2.NoSQL database have dynamic schema. <br />
+                                3.NoSQL database databases are not so good for complex queries. <br />
+
+                            </div>
                         </div>
                     </div>
                 </div>
